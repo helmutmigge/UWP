58c645adcacbf63eb69f17561cc22111a811ab5c
hmsg <hmsg@ITS-HELMUT>
2018-08-23 15:03:55 -0300

    Converter

---
+++ Converter/Converter/BoolToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Converter
+{
+    class BoolToVisibilityConverter : Windows.UI.Xaml.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}
