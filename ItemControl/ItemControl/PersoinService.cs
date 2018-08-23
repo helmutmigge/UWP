e7875e8f855be4e237ae6c357780f08e0b3498c2
hmsg <hmsg@ITS-HELMUT>
2018-08-23 15:07:19 -0300

    ItemControl project

---
+++ ItemControl/ItemControl/PersoinService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemControl
+{
+    class PersoinService
+    {
+
+        public List<Person> People
+        {
+            get
+            {
+                return new List<Person>() {
+                    new Person("Helmut", 35),
+                    new Person("Eduardo", 38),
+                    new Person("Bruno", 32),
+                    new Person("Galvão", 38)
+                };
+            }
+        }
+    }
+}
---
+++ ItemControl/ItemControl/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemControl
+{
+    class Person
+    {
+
+        public Person(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        private string name;
+        public string Name {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return this.age;
+
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
