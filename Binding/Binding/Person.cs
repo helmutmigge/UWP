2527ed3e90a13ad3839fa45a7452e8b539cde267
hmsg <hmsg@ITS-HELMUT>
2018-08-23 15:04:56 -0300

    Binding project

---
+++ Binding/Binding/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binding
+{
+    class Person
+    {
+        private string fullname;
+        public string Fullname {
+            get {
+                return fullname;
+            }
+
+            set
+            {
+                this.fullname = value;
+            }
+        }
+        private string nickname;
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+
+            set
+            {
+                this.nickname = value;
+            }
+        }
+
+    }
+}
+
